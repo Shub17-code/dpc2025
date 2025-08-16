9183de35d7d5fcb029ab6012800571bdc93cac09
Shub17-code <sc241617@gmail.com>
2025-08-16 10:51:24 +0530

    day 2

---
+++ day2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int missingNumber(vector<int>&a, int n) {
+
+    int sum = (n * (n + 1)) / 2;
+
+    int s2 = 0;
+    for (int i = 0; i < n - 1; i++) {
+        s2 += a[i];
+    }
+
+    int missingNum = sum - s2;
+    return missingNum;
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> a = {1, 2, 4, 5};
+    int ans = missingNumber(a, n);
+    cout << "The missing number is: " << ans << endl;
+    return 0;
+}
