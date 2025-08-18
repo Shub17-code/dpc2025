85b2166175d6e33645838dc300356d702afd7f7d
Shub17-code <sc241617@gmail.com>
2025-08-18 17:58:14 +0530

    day 4 done

---
+++ day4.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr1, vector<int> &arr2, int n, int m) {
+
+    int left = n - 1;
+    int right = 0;
+
+    while (left >= 0 && right < m) {
+        if (arr1[left] > arr2[right]) {
+            swap(arr1[left], arr2[right]);
+            left--, right++;
+        }
+        else {
+            break;
+        }
+    }
+
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
+}
+
+int main()
+{
+    vector<int> arr1 = {1, 4, 8, 10};
+    vector<int> arr2 = {2, 3, 9};
+    int n = 4, m = 3;
+    merge(arr1, arr2, n, m);
+    cout << "The merged arrays are: " << "\n";
+    cout << "arr1[] = ";
+    for (int i = 0; i < n; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << "\narr2[] = ";
+    for (int i = 0; i < m; i++) {
+        cout << arr2[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
