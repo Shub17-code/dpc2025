8395511dbdf1e313ae462e1c1fcb9eb0a138fb0e
Shub17-code <sc241617@gmail.com>
2025-08-27 22:23:47 +0530

    Added day 13

---
+++ day13.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string longestPalindrome(string s) {
+    int start=0;
+    int maxLen=1;
+    for(int i=1;i<s.length();i++){
+        int l=i-1;
+        int r=i;
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            if(r-l+1>maxLen){
+                maxLen=r-l+1;
+                start=l;
+            }
+            l--;
+            r++;
+        }
+        l=i-1;
+        r=i+1;
+        while(l>=0 && r<s.length() && s[l]==s[r]){
+            if(r-l+1>maxLen){
+                maxLen=r-l+1;
+                start=l;
+            }
+            l--;
+            r++;
+        }
+    }
+    return s.substr(start,maxLen);
+}
+int main(){
+    string s="babad";
+    cout<<longestPalindrome(s)<<endl;
+    return 0;
+}
